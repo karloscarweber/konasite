c87f19fde83961c385aa413d65f004ce6b2bf1b5
karloscarweber <me@kow.fm>
2023-10-26 22:03:55 -0600

    Add a syntax page and fix a link. And fix syntax highlighting for -- comments.

--- public/index.js
+++ public/index.js
@@ -1,10 +1,134 @@
-/* esm.sh - esbuild bundle(macrolight@1.5.0) es2021 production */
-var b={typescript:["goto","in","instanceof","static","finally","arguments","public","do","else","const","function","class","return","let","catch","eval","for","if","this","try","break","debugger","yield","extends","enum","continue","export","null","switch","private","new","throw","while","case","await","delete","super","default","void","var","protected","package","interface","false","typeof","implements","with","import","true"],c:["auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"],html:["doctype","a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bb","bdo","big","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup","command","datagrid","datalist","dd","del","details","dfn","dialog","dir","div","dl","dt","em","embed","eventsource","fieldset","figcaption","figure","font","footer","form","frame","frameset","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","isindex","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noframes","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","span","strike","strong","style","sub","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","tt","u","ul","var","video","wbr"],python:["False","await","else","import","pass","None","break","except","in","raise","True","class","finally","is","return","and","continue","for","lambda","try","as","def","from","nonlocal","while","assert","del","global","not","with","async","elif","if","or","yield","map","__init__","sum","list","input"],rust:["union","'static","dyn","as","break","const","continue","crate","else","enum","extern","false","fn","for","if","impl","in","let","loop","match","mod","move","mut","pub","ref","return","self","Self","static","struct","super","trait","true","type","unsafe","use","where","while","async","await","dyn","abstract","become","box","do","final","macro","override","priv","typeof","unsized","virtual","yield","try"]},v={python:["py"],typescript:["js","ts","javascript"],rust:["rs"]};for(let[a,i]of Object.entries(v))i.forEach(s=>b[s]=b[a]);var k=a=>a?a.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#39;"):"",x=function(a,i={}){let s={unformatted:"",keyword:"font-weight: bold",punctuation:"color: gray",string:"color: #009bff",comment:"font-style: italic; color: gray"},l=!0;i.dontEscape&&(l=!1),Object.assign(s,i.styles||{});let y=t=>(i.keywords||[]).includes(t),c=typeof a=="object"&&"textContent"in a?a.textContent:a,d=0,u=c[0],r=1,o,f,n="",g="",e=0,m=-1,p=!1,w=t=>!t||e<8&&t==`
-`?!0:[/\S/.test(t),!0,!0,!/[$\w]/.test(t),(o==="/"||o==`
-`)&&p,o==='"'&&p,o==="'"&&p,c[d-4]+f+o=="-->",f+o==="*/",t===`
-`,t===`
-`][e];for(;f=o,o=e<7&&o=="\\"?1:r;){if(r=u,u=c[++d],p=n.length>1,w(r)){if(n){let t="unformatted";e>6?t="comment":e>3?t="string":e<3?t="punctuation":t=y(n)?"keyword":"unformatted";let h=s[t];g+=`<span${h?` style="${h}"`:""} class='macrolight-${t}'>${l?k(n):n}</span>`}m=e&&e<7?e:m,n="",r=="#"?e=10:r+u=="//"?e=9:r+u=="/*"?e=8:r+u+c[d+1]+c[d+2]=="<!--"?e=7:r=="'"?e=6:r=='"'?e=5:r=="/"&&m<2&&o!="<"?e=4:/[$\w]/.test(r)?e=3:/[\])]/.test(r)?e=2:/[\/{}[(\-+*=<>:;|\\.,?!&@~]/.test(r)?e=1:e=0}n+=r}return g},j=(a={},i=".macrolight")=>{let s=document.querySelectorAll(i)||[];Array.from(s).forEach(l=>{l.innerHTML=x(l,a)})};export{b as HL_KEYWORDS,x as highlight,j as highlightAll};
-var highlightAll = j;
+// Snippet from https://github.com/xyzshantaram/campfire/blob/main/dist/campfire.js
+const escape = (str) => {
+    if (!str)
+        return '';
+    return str.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+};
+// ===========================================================================
+
+const highlight = function (src, config = {}) {
+    const styles = {
+        unformatted: '',
+        keyword: 'font-weight: bold',
+        punctuation: 'color: gray',
+        string: 'color: #009bff', // regexes, too
+        comment: 'font-style: italic; color: gray'
+    };
+
+    let shouldEscape = true;
+    if (config.dontEscape) shouldEscape = false;
+
+    Object.assign(styles, config.styles || {});
+
+    const isKeyword = (token) => {
+        const keywords = config.keywords || [];
+        return keywords.includes(token);
+    }
+
+    const text = (typeof src === 'object' && 'textContent' in src) ? src.textContent : src;
+
+    let pos = 0; // current position
+    let next = text[0]; // next character
+    let char = 1; // current character
+    let prev; // previous character
+    let prev_prev; // the one before the previous
+    let token = ''; // current token contents
+    let result = '';
+
+    // current token type:
+    //  0: anything else (whitespaces / newlines)
+    //  1: operator or brace
+    //  2: closing braces (after which '/' is division not regex)
+    //  3: (key)word
+    //  4: regex
+    //  5: string starting with "
+    //  6: string starting with '
+    //  7: xml comment  <!-- -->
+    //  8: multiline comment /* */
+    //  9: single-line comment starting with two slashes //
+    //  9: single-line comment starting with two dashes  --
+    // 10: single-line comment starting with hash #
+    let tokenType = 0;
+
+    // kept to determine between regex and division
+    let lastTokenType = -1;
+    let multichar = false;
+
+    const shouldFinalizeToken = (currentChar) => {
+        if (!currentChar) return true;
+        if (tokenType < 8 && currentChar == '\n') return true;
+
+        // finalization conditions for other tokens
+        return [
+            /\S/.test(currentChar),  // whitespaces get merged.
+            true, // operator, consists of a single character.
+            true, // brace, it consists of a single character.
+            !/[$\w]/.test(currentChar), // (key)word
+            (prev === '/' || prev == '\n') && multichar, // regex
+            prev === '"' && multichar, // string with double-quotes
+            prev === "'" && multichar, // string with single-quotes
+            text[pos - 4] + prev_prev + prev == '-->', // xml comment
+            prev_prev + prev === '*/', // multiline comment
+            currentChar === '\n', // single line comment
+            currentChar === '\n',
+        ][tokenType];
+    }
+
+    // the actual highlighting step
+    while (prev_prev = prev, prev = (tokenType < 7 && prev == '\\') ? 1 : char) {
+        char = next;
+        next = text[++pos];
+        multichar = token.length > 1;
+
+        // checking if current token should be finalized
+        if (shouldFinalizeToken(char)) {
+            if (token) { // remapping token type into style (some types are highlighted similarly)
+                let styleIdx = 'unformatted';
+
+                if (tokenType > 6) styleIdx = 'comment';
+                else if (tokenType > 3) styleIdx = 'string';
+                else if (tokenType < 3) styleIdx = 'punctuation';
+                else styleIdx = isKeyword(token) ? 'keyword' : 'unformatted';
+                const style = styles[styleIdx];
+
+                result += `<span${style ? ` style="${style}"` : ""} class='macrolight-${styleIdx}'>${shouldEscape ? escape(token) : token}</span>`;
+            }
+
+            // saving the previous token type
+            // (skipping whitespaces and comments)
+            lastTokenType = (tokenType && tokenType < 7) ? tokenType : lastTokenType;
+
+            // initializing a new token
+            token = '';
+
+            if (char == '#') tokenType = 10;
+            else if (char + next == '//' || char + next == '--') tokenType = 9;
+            else if (char + next == '/*') tokenType = 8;
+            else if (char + next + text[pos + 1] + text[pos + 2] == '<!--') tokenType = 7;
+            else if (char == "'") tokenType = 6;
+            else if (char == '"') tokenType = 5;
+            else if (char == '/' && (lastTokenType < 2) && prev != '<') tokenType = 4;
+            else if (/[$\w]/.test(char)) tokenType = 3;
+            else if (/[\])]/.test(char)) tokenType = 2;
+            else if ((/[\/{}[(\-+*=<>:;|\\.,?!&@~]/.test(char))) tokenType = 1;
+            else tokenType = 0;
+        }
+
+        token += char;
+    }
+    return result;
+}
+
+const highlightAll = (config = {}, selector = '.macrolight') => {
+    const elts = document.querySelectorAll(selector) || [];
+    Array.from(elts).forEach(elem => {
+        elem.innerHTML = highlight(elem, config);
+    })
+}
 
 // This provides Syntax highlighting, but only a little bit. Nothing fancy.
 highlightAll({
